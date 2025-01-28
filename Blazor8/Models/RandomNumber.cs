5afa1aac048829ac81dcfc32411335e049e37330
Felipe Costa <felipecostasiqu@gmail.com>
2025-01-28 16:03:38 -0300

    Injeção de Dependência Transient

---
+++ Blazor8/Models/RandomNumber.cs
@@ -0,0 +1,13 @@
+﻿namespace Blazor8.Models
+{
+    public class RandomNumber
+    {
+        public int numberIdentify {get; set;}
+
+        public RandomNumber()
+        {
+            Random rdn = new Random();
+            numberIdentify = rdn.Next(0, 10_000);
+        }
+    }
+}
