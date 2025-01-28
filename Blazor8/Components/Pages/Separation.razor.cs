f156da5bd638368deec23a7f224ac285662a6360
Felipe Costa <felipecostasiqu@gmail.com>
2025-01-27 21:55:12 -0300

    Separação CSS

---
+++ Blazor8/Components/Pages/Separation.razor.cs
@@ -0,0 +1,14 @@
+﻿namespace Blazor8.Components.Pages
+{
+    public partial class Separation
+    {
+        /*
+            Razor (Engine Template - ASP.NET): HTML5 + CSS3 + C# + JS
+
+            Blazor: .razor (Componentes Blazor)
+            ASP.NET .cshtml (MVC & Razor Pages): 
+        */
+
+        public string texto = "Olá, Mundo!";
+    }
+}
