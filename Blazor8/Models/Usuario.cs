eb805ba139ee1a5e911980daaf264827fcc69a1c
Felipe Costa <felipecostasiqu@gmail.com>
2025-01-30 11:38:39 -0300

    EditForm

---
+++ Blazor8/Models/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blazor8.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo Email é obrigatório.")]
+        [EmailAddress(ErrorMessage = "Coloque um Email válido.")]
+        public string? Email { get; set; }
+
+        [Required (ErrorMessage = "O campo Senha é obrigatório.")]
+        [MinLength(8, ErrorMessage = "A senha tem que ter no mínimo 8 caracteres.")]
+        public string? Senha { get; set; }
+    }
+}
